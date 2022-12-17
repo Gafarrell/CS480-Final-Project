10850dc5f14572c632c80446aba8774a77624f9f
Jarod-Gobert <78881424+Jarod-Gobert@users.noreply.github.com>
2022-12-17 13:06:53 -0800

    All planets now render
    Increased window size, all planets are rendered, adjusted scale and orbits

--- CS480-Final-Project/graphics.cpp
+++ CS480-Final-Project/graphics.cpp
@@ -77,20 +77,20 @@ bool Graphics::Initialize(int width, int height)
 	m_mercury = new Sphere(48, "assets\\Mercury.jpg");
 	m_mercury->setAngle(vector<float>({ 3 }));
 	m_mercury->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_mercury->setOrbitDistance(vector<float>({ 1.0f,1.0f,1.0f }));
+	m_mercury->setOrbitDistance(vector<float>({ 2.0f,2.0f,2.0f }));
 	m_mercury->setRotationSpeed(vector<float>({ 0.35f }));
-	m_mercury->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_mercury->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_mercury->setScale(vector<float>({ 0.1f,0.1f,0.1f }));
+	m_mercury->setSpeed(vector<float>({ 0.35f, 0.35f, 0.35f }));
 	solarSystem.push_back(m_mercury);
 
 	// Venus
 	m_venus = new Sphere(48, "assets\\Venus.jpg");
 	m_venus->setAngle(vector<float>({ 3 }));
 	m_venus->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_venus->setOrbitDistance(vector<float>({ 2.0f,2.0f,2.0f }));
+	m_venus->setOrbitDistance(vector<float>({ 4.0f,4.0f,4.0f }));
 	m_venus->setRotationSpeed(vector<float>({ 0.35f }));
-	m_venus->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_venus->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_venus->setScale(vector<float>({ 0.25f,0.25f,0.25f }));
+	m_venus->setSpeed(vector<float>({ 0.25f, 0.25f, 0.25f }));
 	solarSystem.push_back(m_venus);
 
 
@@ -98,9 +98,9 @@ bool Graphics::Initialize(int width, int height)
 	m_earth = new Sphere(48, "assets\\2k_earth_daymap.jpg");
 	m_earth->setAngle(vector<float>({ 3 }));
 	m_earth->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_earth->setOrbitDistance(vector<float>({ 3.0f,3.0f,3.0f }));
+	m_earth->setOrbitDistance(vector<float>({ 6.0f,6.0f,6.0f }));
 	m_earth->setRotationSpeed(vector<float>({ 0.35f }));
-	m_earth->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
+	m_earth->setScale(vector<float>({ 0.25f,0.25f,0.25f }));
 	m_earth->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
 	solarSystem.push_back(m_earth);
 
@@ -108,9 +108,9 @@ bool Graphics::Initialize(int width, int height)
 	m_moon = new Sphere(48, "assets\\2k_moon.jpg");
 	m_moon->setAngle(vector<float>({ 0.56 }));
 	m_moon->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new Cos(), new Sin() }));
-	m_moon->setOrbitDistance(vector<float>({ 1.65,1.65,1.65 }));
+	m_moon->setOrbitDistance(vector<float>({ 1.,1.,1. }));
 	m_moon->setRotationSpeed(vector<float>({ 0.5f }));
-	m_moon->setScale(vector<float>({ 0.15f,0.15f,0.15f }));
+	m_moon->setScale(vector<float>({ 0.05f,0.05f,0.05f }));
 	m_moon->setSpeed(vector<float>({ .25f,.25f,.25f }));
 	solarSystem.push_back(m_moon);
 
@@ -118,90 +118,90 @@ bool Graphics::Initialize(int width, int height)
 	m_mars = new Sphere(48, "assets\\Mars.jpg");
 	m_mars->setAngle(vector<float>({ 3 }));
 	m_mars->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_mars->setOrbitDistance(vector<float>({ 4.0f,4.0f }));
+	m_mars->setOrbitDistance(vector<float>({ 8.0f,8.0f,8.0f }));
 	m_mars->setRotationSpeed(vector<float>({ 0.35f }));
-	m_mars->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_mars->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_mars->setScale(vector<float>({ 0.18f,0.18f,0.18f }));
+	m_mars->setSpeed(vector<float>({ 0.08f, 0.08f, 0.08f }));
 	solarSystem.push_back(m_mars);
 
 	// Ceres
 	m_ceres = new Sphere(48, "assets\\Ceres.jpg");
 	m_ceres->setAngle(vector<float>({ 3 }));
 	m_ceres->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_ceres->setOrbitDistance(vector<float>({ 5.0f,5.0f,5.0f }));
+	m_ceres->setOrbitDistance(vector<float>({ 10.0f,10.0f,10.0f }));
 	m_ceres->setRotationSpeed(vector<float>({ 0.35f }));
-	m_ceres->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_ceres->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_ceres->setScale(vector<float>({ 0.05f,0.05f,0.05f }));
+	m_ceres->setSpeed(vector<float>({ 0.07f, 0.07f, 0.07f }));
 	solarSystem.push_back(m_ceres);
 
 	// Jupiter
 	m_jupiter = new Sphere(48, "assets\\Jupiter.jpg");
 	m_jupiter->setAngle(vector<float>({ 3 }));
 	m_jupiter->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_jupiter->setOrbitDistance(vector<float>({ 6.0f,6.0f,6.0f }));
+	m_jupiter->setOrbitDistance(vector<float>({ 12.0f,12.0f,12.0f }));
 	m_jupiter->setRotationSpeed(vector<float>({ 0.35f }));
 	m_jupiter->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_jupiter->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_jupiter->setSpeed(vector<float>({ 0.06f, 0.06f, 0.06f }));
 	solarSystem.push_back(m_jupiter);
 
 	// Saturn
 	m_saturn = new Sphere(48, "assets\\Saturn.jpg");
 	m_saturn->setAngle(vector<float>({ 3 }));
 	m_saturn->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_saturn->setOrbitDistance(vector<float>({ 7.0f,7.0f,7.0f }));
+	m_saturn->setOrbitDistance(vector<float>({ 14.0f,14.0f,14.0f }));
 	m_saturn->setRotationSpeed(vector<float>({ 0.35f }));
-	m_saturn->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_saturn->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_saturn->setScale(vector<float>({ 0.4f,0.4f,0.4f }));
+	m_saturn->setSpeed(vector<float>({ 0.05f, 0.05f, 0.05f }));
 	solarSystem.push_back(m_saturn);
 
 	// Uranus
 	m_uranus = new Sphere(48, "assets\\Uranus.jpg");
 	m_uranus->setAngle(vector<float>({ 3 }));
 	m_uranus->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_uranus->setOrbitDistance(vector<float>({ 8.0f,8.0f,8.0f }));
+	m_uranus->setOrbitDistance(vector<float>({ 16.0f,16.0f,16.0f }));
 	m_uranus->setRotationSpeed(vector<float>({ 0.35f }));
-	m_uranus->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_uranus->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_uranus->setScale(vector<float>({ 0.3f,0.3f,0.3f }));
+	m_uranus->setSpeed(vector<float>({ 0.03f, 0.03f, 0.03f }));
 	solarSystem.push_back(m_uranus);
 
 	// Neptune
 	m_neptune = new Sphere(48, "assets\\Neptune.jpg");
 	m_neptune->setAngle(vector<float>({ 3 }));
 	m_neptune->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_neptune->setOrbitDistance(vector<float>({ 9.0f,9.0f,9.0f }));
+	m_neptune->setOrbitDistance(vector<float>({ 18.0f,18.0f,18.0f }));
 	m_neptune->setRotationSpeed(vector<float>({ 0.35f }));
-	m_neptune->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_neptune->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_neptune->setScale(vector<float>({ 0.3f,0.3f,0.3f }));
+	m_neptune->setSpeed(vector<float>({ 0.02f, 0.02f, 0.02f }));
 	solarSystem.push_back(m_neptune);
 
 	// Pluto
 	m_pluto = new Sphere(48, "assets\\Mercury.jpg");
 	m_pluto->setAngle(vector<float>({ 3 }));
 	m_pluto->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_pluto->setOrbitDistance(vector<float>({ 10.0f,10.0f,10.0f }));
+	m_pluto->setOrbitDistance(vector<float>({ 20.0f,20.0f,20.0f }));
 	m_pluto->setRotationSpeed(vector<float>({ 0.35f }));
-	m_pluto->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_pluto->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_pluto->setScale(vector<float>({ 0.05f,0.05f,0.05f }));
+	m_pluto->setSpeed(vector<float>({ 0.01f, 0.01f, 0.01f }));
 	solarSystem.push_back(m_pluto);
 
 	// Haumea
 	m_haumea = new Sphere(48, "assets\\Haumea.jpg");
 	m_haumea->setAngle(vector<float>({ 3 }));
 	m_haumea->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_haumea->setOrbitDistance(vector<float>({ 11.0f,11.0f,11.0f }));
+	m_haumea->setOrbitDistance(vector<float>({ 22.0f,22.0f,22.0f }));
 	m_haumea->setRotationSpeed(vector<float>({ 0.35f }));
-	m_haumea->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_haumea->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_haumea->setScale(vector<float>({ 0.05f,0.05f,0.05f }));
+	m_haumea->setSpeed(vector<float>({ 0.008f, 0.008f, 0.008f }));
 	solarSystem.push_back(m_haumea);
 
 	// Eris
 	m_eris = new Sphere(48, "assets\\Eris.jpg");
 	m_eris->setAngle(vector<float>({ 3 }));
 	m_eris->setOrbitalFunctions(std::vector<TrigFunction*>({ new Sin(), new None(), new Cos() }));
-	m_eris->setOrbitDistance(vector<float>({ 12.0f,12.0f,12.0f }));
+	m_eris->setOrbitDistance(vector<float>({ 24.0f,24.0f,24.0f }));
 	m_eris->setRotationSpeed(vector<float>({ 0.35f }));
-	m_eris->setScale(vector<float>({ 0.5f,0.5f,0.5f }));
-	m_eris->setSpeed(vector<float>({ 0.15f, 0.15f, 0.15f }));
+	m_eris->setScale(vector<float>({ 0.05f,0.05f,0.05f }));
+	m_eris->setSpeed(vector<float>({ 0.006f, 0.006f, 0.006f }));
 	solarSystem.push_back(m_eris);
 
 
@@ -226,6 +226,7 @@ void Graphics::HierarchicalUpdate2(double dt) {
 
 	glm::mat4 tmat, rmat, smat;
 
+	//Sun transform
 	ComputeTransforms(totalTime, m_sun->getOrbitalFunctions(), m_sun->getSpeed(), m_sun->getDistance(), m_sun->getRotationSpeed(), glm::vec3(0, 1, 0), m_sun->getScale(), tmat, rmat, smat);
 	modelStack.push(modelStack.top());
 
@@ -237,7 +238,33 @@ void Graphics::HierarchicalUpdate2(double dt) {
 
 	modelStack.pop();
 
+	//Mercury transform
+	ComputeTransforms(totalTime, m_mercury->getOrbitalFunctions(), m_mercury->getSpeed(), m_mercury->getDistance(), m_mercury->getRotationSpeed(), glm::vec3(0, 1, 0), m_mercury->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
 
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_mercury->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Venus transform
+	ComputeTransforms(totalTime, m_venus->getOrbitalFunctions(), m_venus->getSpeed(), m_venus->getDistance(), m_venus->getRotationSpeed(), glm::vec3(0, 1, 0), m_venus->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_venus->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Earth transform
 	ComputeTransforms(totalTime, m_earth->getOrbitalFunctions(), m_earth->getSpeed(), m_earth->getDistance(), m_earth->getRotationSpeed(), glm::vec3(0, 1, 0), m_earth->getScale(), tmat, rmat, smat);
 	modelStack.push(modelStack.top());
 
@@ -249,7 +276,7 @@ void Graphics::HierarchicalUpdate2(double dt) {
 
 	modelStack.pop();
 
-
+	//Moon transform
 	ComputeTransforms(totalTime, m_moon->getOrbitalFunctions(), m_moon->getSpeed(), m_moon->getDistance(), m_moon->getRotationSpeed(), glm::vec3(0, 1, 0), m_moon->getScale(), tmat, rmat, smat);
 	modelStack.push(modelStack.top());
 
@@ -259,6 +286,127 @@ void Graphics::HierarchicalUpdate2(double dt) {
 	modelStack.top() *= smat;
 
 	m_moon->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+	modelStack.pop();
+
+	//Mars transform
+	ComputeTransforms(totalTime, m_mars->getOrbitalFunctions(), m_mars->getSpeed(), m_mars->getDistance(), m_mars->getRotationSpeed(), glm::vec3(0, 1, 0), m_mars->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_mars->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Ceres transform
+	ComputeTransforms(totalTime, m_ceres->getOrbitalFunctions(), m_ceres->getSpeed(), m_ceres->getDistance(), m_ceres->getRotationSpeed(), glm::vec3(0, 1, 0), m_ceres->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_ceres->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Jupiter transform
+	ComputeTransforms(totalTime, m_jupiter->getOrbitalFunctions(), m_jupiter->getSpeed(), m_jupiter->getDistance(), m_jupiter->getRotationSpeed(), glm::vec3(0, 1, 0), m_jupiter->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_jupiter->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Saturn transform
+	ComputeTransforms(totalTime, m_saturn->getOrbitalFunctions(), m_saturn->getSpeed(), m_saturn->getDistance(), m_saturn->getRotationSpeed(), glm::vec3(0, 1, 0), m_saturn->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_saturn->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Uranus transform
+	ComputeTransforms(totalTime, m_uranus->getOrbitalFunctions(), m_uranus->getSpeed(), m_uranus->getDistance(), m_uranus->getRotationSpeed(), glm::vec3(0, 1, 0), m_uranus->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_uranus->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Nepture transform
+	ComputeTransforms(totalTime, m_neptune->getOrbitalFunctions(), m_neptune->getSpeed(), m_neptune->getDistance(), m_neptune->getRotationSpeed(), glm::vec3(0, 1, 0), m_neptune->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_neptune->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Pluto transform
+	ComputeTransforms(totalTime, m_pluto->getOrbitalFunctions(), m_pluto->getSpeed(), m_pluto->getDistance(), m_pluto->getRotationSpeed(), glm::vec3(0, 1, 0), m_pluto->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_pluto->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Huamea transform
+	ComputeTransforms(totalTime, m_haumea->getOrbitalFunctions(), m_haumea->getSpeed(), m_haumea->getDistance(), m_haumea->getRotationSpeed(), glm::vec3(0, 1, 0), m_haumea->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_haumea->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
+
+	//Eris transform
+	ComputeTransforms(totalTime, m_eris->getOrbitalFunctions(), m_eris->getSpeed(), m_eris->getDistance(), m_eris->getRotationSpeed(), glm::vec3(0, 1, 0), m_eris->getScale(), tmat, rmat, smat);
+	modelStack.push(modelStack.top());
+
+	modelStack.top() *= tmat;
+	modelStack.push(modelStack.top());
+	modelStack.top() *= rmat * smat;
+
+	m_eris->Update(modelStack.top());
+
+	modelStack.pop();
+	modelStack.pop();
 
 	while (!modelStack.empty()) modelStack.pop();
 }
