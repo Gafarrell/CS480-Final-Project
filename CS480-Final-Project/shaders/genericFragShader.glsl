4ab4e61f304ba3b74d25a0d46bf4d10a9f2b6385
Jarod-Gobert <78881424+Jarod-Gobert@users.noreply.github.com>
2022-12-17 19:22:54 -0800

    vertex and frag shader changes

--- CS480-Final-Project/shaders/genericFragShader.glsl
+++ CS480-Final-Project/shaders/genericFragShader.glsl
@@ -4,14 +4,27 @@ uniform sampler2D sp;
 
 in vec3 color; 
 in vec2 tc;
+
+in vec3 Normal;
+in vec3 crntPos;
+
+uniform vec4 lightColor;
+uniform vec3 lightPos;
+
 uniform bool hasTexture;
+
 
 out vec4 frag_color; 
 
 void main(void) 
 { 
+    vec3 normal = normalize(Normal);
+    vec3 lightDirection = normalize(lightPos - crntPos);
+    float diffuse = max(dot(normal, lightDirection),0.0f);
+
+
     if(hasTexture)
-        frag_color = texture(sp, tc);
+        frag_color = texture(sp, tc) * lightColor * diffuse;
     else 
         frag_color = vec4(color.rgb, 1);
 }
--- CS480-Final-Project/shaders/genericVertShader.glsl
+++ CS480-Final-Project/shaders/genericVertShader.glsl
@@ -3,9 +3,14 @@
 layout (location = 0) in vec3 v_position; 
 layout (location = 1) in vec3 v_color; 
 layout (location = 2) in vec2 v_tc;  
+
+layout(location = 3) in vec3 aNormal;
     
 out vec3 color; 
 out vec2 tc;
+
+out vec3 Normal;
+out vec3 crntPos;
 
 uniform mat4 projectionMatrix; 
 uniform mat4 viewMatrix; 
@@ -15,8 +20,11 @@ uniform sampler2D sp;
 
 void main(void) 
 { 
-    vec4 v = vec4(v_position, 1.0); 
+    crntPos = vec3(modelMatrix *vec4(v_position, 1.0f));
+
+    vec4 v = vec4(crntPos, 1.0); 
     gl_Position = (projectionMatrix * viewMatrix * modelMatrix) * v; 
     color = v_color; 
     tc = v_tc;
+    Normal = aNormal;
 } 
